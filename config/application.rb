5b404a1aed0df60ab04ac8df4f402b7f824b5b66
Sanad <s_wj_88@hotmail.com>
2021-06-14 07:10:31 +0300

    Modify the controller

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,10 @@
 require 'rulers'
-
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require_relative '../app/controllers/quotes_controller.rb'
 module BestQuotes
   class Application < Rulers::Application
-
   end
 end
+
+
+
