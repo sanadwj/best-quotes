4d5762c2d7db3f5d524a6797367cd5b9150a6e24
Sanad <s_wj_88@hotmail.com>
2021-06-25 23:11:41 +0300

    Add model and controller actions

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,16 @@ class QuotesController < Speses::Controller
     render :a_quote, :noun => :winking
   end
 
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
   def exception
     raise "It's a bad one!"
   end
